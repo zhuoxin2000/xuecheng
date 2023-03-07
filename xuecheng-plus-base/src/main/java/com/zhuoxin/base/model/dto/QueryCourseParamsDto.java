e939da00f7cac3d868319d58678167ea347b3ad1
zhuo9311 <931196505.com>
2023-03-07 16:02:18 +0800

    项目初始架构

---
+++ xuecheng-plus-base/src/main/java/com/zhuoxin/base/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,21 @@
+package com.zhuoxin.base.model.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @description 课程查询参数Dto
+ * @author Mr.M
+ * @date 2022/9/6 14:36
+ * @version 1.0
+ */
+@Data
+@ToString
+public class QueryCourseParamsDto {
+    //审核状态
+    private String auditStatus;
+    //课程名称
+    private String courseName;
+    //发布状态
+    private String publishStatus;
+}
